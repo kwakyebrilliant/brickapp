eeb35ff323a7769c06b3a31c1afbc3b1c8d57be5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 13:01:23 +0000

    home updates

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,26 +14,47 @@ class _HomePageState extends State<HomePage> {
   double ballX = 0;
   double ballY = 0;
 
+  //startGame method
+  void startGame() {
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        ballY -= 0.1;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal[100],
-      body: Center(
-        child: Stack(
-          children: [
-            //ball
-            Container(
-              alignment: const Alignment(0, 0),
-              child: Container(
-                height: 15.0,
-                width: 15.0,
-                decoration: const BoxDecoration(
-                  color: Colors.teal,
-                  shape: BoxShape.circle,
+    return GestureDetector(
+      onTap: startGame,
+      child: Scaffold(
+        backgroundColor: Colors.teal[100],
+        body: Center(
+          child: Stack(
+            children: [
+              //tap to play'
+              Container(
+                alignment: Alignment(0, -0.2),
+                child: Text(
+                  'Tap to play',
+                  style: TextStyle(color: Colors.teal[500]),
                 ),
               ),
-            ),
-          ],
+
+              //ball
+              Container(
+                alignment: Alignment(ballX, ballY),
+                child: Container(
+                  height: 15.0,
+                  width: 15.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.teal,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
