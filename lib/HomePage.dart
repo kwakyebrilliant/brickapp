f09ed93d46b30274022cedb70fc38af9ffff04b8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 16:22:51 +0000

    homepage updates

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,7 @@ import 'package:brickapp/Coverscreen.dart';
 import 'package:brickapp/Player.dart';
 import 'package:brickapp/ball.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -34,32 +35,63 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  //move player left
+  void moveLeft() {
+    setState(() {
+      //move left if moving left doesn't move player off the screen
+      if (!(playerX - 0.2 <= -1)) {
+        playerX -= 0.2;
+      }
+    });
+  }
+
+  //move player right
+  void moveRight() {
+    setState(() {
+      //move right if moving right doesn't move player off the screen
+      if (!(playerX + 0.2 >= 1)) {
+        playerX += 0.2;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: startGame,
-      child: Scaffold(
-        backgroundColor: Colors.teal[100],
-        body: Center(
-          child: Stack(
-            children: [
-              //tap to play
-              CoverScreen(
-                hasGameStarted: hasGameStarted,
-              ),
+    return KeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKeyEvent: (event) {
+        if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+          moveLeft();
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+          moveRight();
+        }
+      },
+      child: GestureDetector(
+        onTap: startGame,
+        child: Scaffold(
+          backgroundColor: Colors.teal[100],
+          body: Center(
+            child: Stack(
+              children: [
+                //tap to play
+                CoverScreen(
+                  hasGameStarted: hasGameStarted,
+                ),
 
-              //ball
-              MyBall(
-                ballX: ballX,
-                ballY: ballY,
-              ),
+                //ball
+                MyBall(
+                  ballX: ballX,
+                  ballY: ballY,
+                ),
 
-              //player
-              MyPlayer(
-                player: playerX,
-                playerwidth: playerwidth,
-              ),
-            ],
+                //player
+                MyPlayer(
+                  player: playerX,
+                  playerwidth: playerwidth,
+                ),
+              ],
+            ),
           ),
         ),
       ),
