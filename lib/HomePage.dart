68411ca27f18cff9f4d58c1da6a117db349bdce0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 12:44:54 +0000

    home updates

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,6 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.teal[100],
+      body: Center(
+        child: Stack(
+          children: [
+            //ball
+            Container(
+              alignment: const Alignment(0, 0),
+              child: Container(
+                height: 15.0,
+                width: 15.0,
+                decoration: const BoxDecoration(
+                  color: Colors.teal,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
