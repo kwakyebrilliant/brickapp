a572c3b942a68ce6647727d65f00f79f5dd00590
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 14:36:28 +0000

    player updates

--- lib/Player.dart
+++ lib/Player.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MyPlayer extends StatelessWidget {
-  const MyPlayer({super.key});
+  const MyPlayer({super.key, this.player, this.playerwidth});
+
+  final player;
+  final playerwidth;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment(player, 0.0),
+      child: Container(
+        height: 10.0,
+        width: MediaQuery.of(context).size.width * playerwidth / 2,
+        color: Colors.teal,
+      ),
+    );
   }
 }
