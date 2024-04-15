114d4ec3ce56a6b358a5bd794aae4d717497b410
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 14:24:05 +0000

    ball added

---
+++ lib/ball.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyBall extends StatelessWidget {
+  const MyBall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
