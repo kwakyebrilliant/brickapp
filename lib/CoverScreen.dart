bb03cb2f5b834189ef56445179aac4ba73d51035
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 14:12:51 +0000

    coverscreen added

---
+++ lib/CoverScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CoverScreen extends StatelessWidget {
+  const CoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
