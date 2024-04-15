ee21bce3c1200dfd4348d3e271734bb7f8d0a819
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-15 14:16:59 +0000

    coverscreen updates

--- lib/CoverScreen.dart
+++ lib/CoverScreen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CoverScreen extends StatelessWidget {
-  const CoverScreen({super.key});
+  const CoverScreen({super.key, required this.hasGameStarted});
+
+  final hasGameStarted;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return hasGameStarted
+        ? Container()
+        : Container(
+            alignment: const Alignment(0, -0.2),
+            child: Text(
+              'Tap to play',
+              style: TextStyle(color: Colors.teal[500]),
+            ),
+          );
   }
 }
