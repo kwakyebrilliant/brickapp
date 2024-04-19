5173710808e18ecbcb87fc4b7bdf459785b7dfc5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-19 13:51:01 +0000

    player updates

--- lib/Player.dart
+++ lib/Player.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MyPlayer extends StatelessWidget {
-  const MyPlayer({super.key, this.playerX, this.playerwidth});
+  const MyPlayer({super.key, this.playerX, this.playerWidth});
 
   final playerX;
-  final playerwidth;
+  final playerWidth;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment:
-          Alignment((2 * playerX + playerwidth) / (2 - playerwidth), 0.9),
+          Alignment((2 * playerX + playerWidth) / (2 - playerWidth), 0.9),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10.0),
         child: Container(
           height: 10.0,
-          width: MediaQuery.of(context).size.width * playerwidth / 2,
+          width: MediaQuery.of(context).size.width * playerWidth / 2,
           color: Colors.teal,
         ),
       ),
