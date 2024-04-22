1c7bb47fb2e359654c546a6a3ca5591617d77ccd
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-22 12:37:02 +0000

    brick updates

--- lib/Brick.dart
+++ lib/Brick.dart
@@ -19,7 +19,8 @@ class MyBrick extends StatelessWidget {
     return brickBroken
         ? Container()
         : Container(
-            alignment: Alignment(brickX, brickY),
+            alignment:
+                Alignment((2 * brickX + brickWidth) / (2 - brickWidth), brickY),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(5.0),
               child: Container(
