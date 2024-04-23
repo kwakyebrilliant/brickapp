9afb683747610c10bfc949be95db7651a3641a27
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-23 09:38:39 +0000

    coverscreen updates

--- lib/CoverScreen.dart
+++ lib/CoverScreen.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CoverScreen extends StatelessWidget {
   const CoverScreen({super.key, required this.hasGameStarted});
 
-  final hasGameStarted;
+  final bool hasGameStarted;
+
+  //font
+  static var gameFont = GoogleFonts.pressStart2p(
+      textStyle: const TextStyle(
+          color: Colors.teal, letterSpacing: 0, fontSize: 28.0));
 
   @override
   Widget build(BuildContext context) {
     return hasGameStarted
         ? Container()
-        : Container(
-            alignment: const Alignment(0, -0.2),
-            child: Text(
-              'Tap to play',
-              style: TextStyle(color: Colors.teal[500]),
-            ),
+        : Stack(
+            children: [
+              Container(
+                alignment: const Alignment(0, -0.5),
+                child: Text(
+                  'BRICKAPP',
+                  style: gameFont,
+                ),
+              ),
+              Container(
+                alignment: const Alignment(0, -0.1),
+                child: Text(
+                  'Tap to play',
+                  style: TextStyle(color: Colors.teal[500]),
+                ),
+              ),
+            ],
           );
   }
 }
